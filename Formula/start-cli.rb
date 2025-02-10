41019c0dd5b5ea4e32a9e1ca127e5fe00b6a98f5
Mariusz Kogen <k0gen@pm.me>
2025-02-10 08:09:26 +0100

    alpha14 bump

--- Formula/start-cli.rb
+++ Formula/start-cli.rb
@@ -5,20 +5,20 @@ class StartCli < Formula
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/start9labs/start-cli/releases/download/v0.3.6-alpha13/start-cli-aarch64-apple-darwin"
+      url "https://github.com/start9labs/start-cli/releases/download/v0.3.6-alpha14/start-cli-aarch64-apple-darwin"
       sha256 "f28a31ee12bbbf669d04e16ef60bb89a93631eea699afe7f051d35345af2f649"
     else
-      url "https://github.com/start9labs/start-cli/releases/download/v0.3.6-alpha13/start-cli-x86_64-apple-darwin"
+      url "https://github.com/start9labs/start-cli/releases/download/v0.3.6-alpha14/start-cli-x86_64-apple-darwin"
       sha256 "f29d5fc2ae8abd28c48ad4a4f4245279e48947c7d7f22f997de1f7b52b35f45c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/start9labs/start-cli/releases/download/v0.3.6-alpha13/start-cli-aarch64-unknown-linux-gnu"
+      url "https://github.com/start9labs/start-cli/releases/download/v0.3.6-alpha14/start-cli-aarch64-unknown-linux-gnu"
       sha256 "4b65a34ede46f87d2a46d7c5ada30ff8782d3a40ec9417a2ce7832fc902409e6"
     else
-      url "https://github.com/start9labs/start-cli/releases/download/v0.3.6-alpha13/start-cli-x86_64-unknown-linux-gnu"
+      url "https://github.com/start9labs/start-cli/releases/download/v0.3.6-alpha14/start-cli-x86_64-unknown-linux-gnu"
       sha256 "80bc9d8c7214005a1bdc6c1a4f444c4bb423a6f285162ab46a282b0fa2f9c7f8"
     end
   end
